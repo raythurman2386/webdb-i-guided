85d5364d651f617235d845c9e4fce2f836623449
raythurman2386 <raymondthurman5@gmail.com>
2019-12-17 21:40:17 -0500

    final lecture

--- posts/post-router.js
+++ posts/post-router.js
@@ -1,26 +1,51 @@
-const express = require("express")
-const db = require("../data/db-config") // database access using knex
+const postRouter = require("express").Router();
+const db = require("../data/db-config"); // database access using knex
 
-const router = express.Router()
+postRouter
 
-router.get("/", (req, res, next) => {
+  .get("/", async (req, res, next) => {
+    try {
+      const posts = await db('posts')
+      return res.status(200).json(posts)
+    } catch (error) {
+      next(error)
+    }
+  })
 
-})
+  .get("/:id", async (req, res, next) => {
+    try {
+      const post = await db('posts').where({ id: req.params.id }).first()
+      res.status(200).json(post)
+    } catch (error) {
+      next(error)
+    }
+  })
 
-router.get("/:id", (req, res, next) => {
+  .post("/", async (req, res, next) => {
+    try {
+      const [post] = await db('posts').insert({ title: req.body.title, contents: req.body.contents });
+      res.status(201).json(await db('posts').where("id", post).first());
+    } catch (error) {
+      next(error)
+    }
+  })
 
-})
+  .put("/:id", async (req, res, next) => {
+    try {
+      const post = await db('posts').where({ id: req.params.id }).update({ title: req.body.title, contents: req.body.contents });
+      res.status(200).json(await db('posts').where("id", req.params.id).first())
+    } catch (error) {
+      next(error)
+    }
+  })
 
-router.post("/", (req, res, next) => {
+  .delete("/:id", async (req, res, next) => {
+    try {
+      const post = await db('posts').where({ id: req.params.id }).delete();
+      res.status(204).end()
+    } catch (error) {
+      next(error)
+    }
+  });
 
-})
-
-router.put("/:id", (req, res, next) => {
-
-})
-
-router.delete("/:id", (req, res, next) => {
-
-})
-
-module.exports = router
+module.exports = postRouter;
